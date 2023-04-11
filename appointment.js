fa02b72d846405469a9e9056d88faf45a31ccfe9
navadeep1012 <navadeep1012@gmail.com>
2023-04-11 15:06:26 +0530

    making registration page dynamic final commit

--- appointment.js
+++ appointment.js
@@ -3,12 +3,8 @@ let email = document.getElementById('email');
 let contact = document.getElementById('contact');
 let items = document.getElementById('items');
 let form = document.getElementById('form');
-
 form.addEventListener('submit',onSubmit);
 items.addEventListener('click',clicked);
-
-
-
 function onSubmit(e){
     e.preventDefault();
     const obj = {
@@ -16,13 +12,29 @@ function onSubmit(e){
         email : email.value,
         contact : contact.value
     }
+    const id = form.getAttribute('data-id');
 
-    axios.post('https://crudcrud.com/api/bcaa1d4d2b774adfbb47251ca06028a1/appointmentData',obj)
-    .then(res => {
-        showOutput(res.data);
-    })
-    .catch(err => console.log(err));
+    if(id){
+        axios.put(`https://crudcrud.com/api/bcaa1d4d2b774adfbb47251ca06028a1/appointmentData/${id}`,obj)
+        .then ( (res) => {
+            const li = items.querySelector(`[data-id="${id}"]`);
+            li.innerText = `${obj.name} - ${obj.email} ${obj.contact} `;
+            addFunctionality(li);
+
+            form.removeAttribute('data-id');
+        })
+        .catch( (err) => console.log(err));
+    }
+    else{
+        axios.post('https://crudcrud.com/api/bcaa1d4d2b774adfbb47251ca06028a1/appointmentData',obj)
+        .then(res => {
+            showOutput(res.data);
+        })
+        .catch(err => console.log(err));
+    }
+
 }
+
 window.addEventListener("DOMContentLoaded",() => {
     axios.get('https://crudcrud.com/api/bcaa1d4d2b774adfbb47251ca06028a1/appointmentData')
     .then( (res) => {
@@ -30,40 +42,64 @@ window.addEventListener("DOMContentLoaded",() => {
             showOutput(res.data[i]);
         }
     })
+    .catch( (err) => console.log(err));
 })
-
-
-
 
 function showOutput(data){
     let li = document.createElement('li');
-    li.innerText = `${data.name} - ${data.email} ${data.contact}`;
+    li.innerText = `${data.name} - ${data.email} ${data.contact} `;
     li.className = "list-group-item mb-2";
-    let button = document.createElement('button');
-    button.className = "delete";
-    button.setAttribute('data-id',data._id)
-
-    button.style.float="right";
-    button.innerText = "delete";
-    li.appendChild(button);
-
-    let edit = document.createElement('button');
-    edit.className = "edit mr-2";
-    edit.innerText = "edit";
-    edit.style.float = "right"; 
-    li.appendChild(edit);
+    li.setAttribute('data-id',data._id);
+    addFunctionality(li);
     items.appendChild(li);
 }
 
+
 function clicked(e){
     if(e.target.classList.contains('delete')){
-        if(confirm('Are you sure?')){
-            const li = e.target.parentElement;
-            const id =  li.querySelector('.delete').getAttribute('data-id');
-            axios.delete(`https://crudcrud.com/api/bcaa1d4d2b774adfbb47251ca06028a1/appointmentDatagit/${id}`)
-            .then( (res)=>console.log('success'))
-            .catch( (err) => console.log(err));
-            items.removeChild(li);
-        }
+       deleteItem(e.target.parentElement);
     }
+
+    if(e.target.classList.contains('edit')){
+        editItem(e.target.parentElement);
+    }
+}
+
+
+
+function deleteItem(li){
+    if(confirm('Are you sure?')){
+        const id =  li.getAttribute('data-id');
+        axios.delete(`https://crudcrud.com/api/bcaa1d4d2b774adfbb47251ca06028a1/appointmentData/${id}`)
+        .then( (res)=>console.log('success'))
+        .catch( (err) => console.log(err));
+        items.removeChild(li);
+    }
+}
+
+
+function editItem(li){
+    const arr = li.textContent.split(/[ ]+/);
+    name.value = arr[0];
+    email.value = arr[2];
+    contact.value = arr[3];
+
+    form.setAttribute('data-id',li.getAttribute('data-id'));
+}
+
+function addFunctionality(li){
+    let button = document.createElement('button');
+    button.className = "delete";
+    button.style.float="right";
+    button.innerText = "delete";
+    li.appendChild(button);
+    let edit = document.createElement('button');
+    edit.className = "edit mr-2";
+    
+        edit.innerText = "edit";
+        edit.style.float = "right"; 
+        li.appendChild(edit);
+        edit.innerText = "edit";
+    edit.style.float = "right"; 
+    li.appendChild(edit);
 }
